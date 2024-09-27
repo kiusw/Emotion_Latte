b1e2129e5d0c141872fd34c715c17d6c3dc5ff53
Bleedbread <seng.bin42@gmail.com>
2024-09-27 14:07:37 +0900

    first

---
+++ Desktop/EmotionLatte-master/src/component/autoSlider.js
@@ -0,0 +1,16 @@
+import '../css/autoSlider.css'
+
+function AutoSlider() {
+  return(
+      <div className="autoSlider">
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+  )
+};
+
+export default AutoSlider;
---
+++ Desktop/EmotionLatte-master/src/component/section.js
@@ -0,0 +1,14 @@
+import '../css/section.css'
+
+function Section() {
+  return(
+    <section>
+      <input type="checkbox" value="아이디" id="saveIdCheckBox" />
+      <div id="autoLogin">자동 로그인</div>
+      <input type="checkbox" value="비밀번호 저장" id="savePasswordCheckBox" />
+      <div id="savePassword">비밀번호 저장</div>
+    </section>
+  )
+}
+
+export default Section;
